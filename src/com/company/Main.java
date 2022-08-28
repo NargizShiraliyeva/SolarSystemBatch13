a316ac7b9b3f74e1ae83c618fbfcb056bcc545e9
NargizShiraliyeva <shiraliyeva.nargiz@gmail.com>
2022-08-28 11:15:59 -0400

    feature for planet2 has been added with 0 errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,6 +17,12 @@ public class Main {
         omaro.name = "ferro";
         omaro.radius = 12344555;
         omaro.color = "pink";
+
+        //    planet2
+        Planet2 superwomen = new Planet2();
+        superwomen.size="766585857km";
+        superwomen.name="pliea";
+        superwomen.color="green";
     }
 }
 
